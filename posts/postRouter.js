04be2fe5daa9681cc846b1450e6f6bb53b001225
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-02 14:16:55 -0400

    post route get works

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,29 @@
-const express = 'express';
+const express = require("express");
+DB = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  DB.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: "The post information cannot be retrueved",
+        message: err.message
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
-};
+function validatePostId(req, res, next) {}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express");
-
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+const helmet = require("helmet"); //yarn added helmet for better security
+const morgan = require("morgan"); // added morgan
 const server = express();
 
-server.get("/", (req, res) => {
+server.use(express.json());
+
+server.use(morgan("dev"));
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+server.use(helmet());
+server.use(logger);
+
+server.get("/", (req, res, next) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+
+  next();
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const date = new Date();
+  const method = req.method;
+  const url = req.url;
+  console.log(`${date}, ${method}, ${url} it works!`);
+  next();
+}
 
 module.exports = server;
